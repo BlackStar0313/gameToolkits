753806015f2c56910ac02e0bddb000256cacdf0b
zhiwei.liu <270274978@qq.com>
2017-10-13 13:17:29 +0800

    [CtrRadarChart] add test code ,optimize code

--- src/GameMainscene.ts
+++ src/GameMainscene.ts
@@ -41,9 +41,33 @@ class GameMainscene extends egret.DisplayObjectContainer{
 		// //>>>>>>>>>>>>>>>>>>
 
 		//test CtrRadarChart
-		let ctrRaderChart: CtrRadarChart = new CtrRadarChart();
+		//<<<<<<<<<<<<<<<<<<<<<<<<<<<
+		let dataArray: Array<CtrRadarChartPropertyData> = [] ; 
+		let data1:CtrRadarChartPropertyData = { name: "胸", currentValue: 90 , maxValue: 100 , nameColor: 0xeeffcc , valueColor: 0xeeff00 ,labelSize: 30};
+		let data2 = { name: "屁股", currentValue: 80 , maxValue: 100 , nameColor: 0xeecccc , valueColor: 0xeeff00 ,labelSize: 30};
+		let data3 = { name: "腰", currentValue: 60 , maxValue: 100 , nameColor: 0xeebbcc , valueColor: 0xeeff00 ,labelSize: 30};
+		let data4 = { name: "腿", currentValue: 20 , maxValue: 100 , nameColor: 0xeeaacc , valueColor: 0xeeff00 ,labelSize: 30};
+		let data5 = { name: "手", currentValue: 40 , maxValue: 100 , nameColor: 0xaaffcc , valueColor: 0xeeff00 ,labelSize: 30};
+		let data6 = { name: "脸", currentValue: 50 , maxValue: 100 , nameColor: 0xbbffcc , valueColor: 0xeeff00 ,labelSize: 30};
+		dataArray.push(data1);
+		dataArray.push(data2);
+		dataArray.push(data3);
+		dataArray.push(data4);
+		dataArray.push(data5);
+		dataArray.push(data6);
+		let ctrRaderChart: CtrRadarChart = new CtrRadarChart(dataArray, true );
 		ctrRaderChart.x = 300 ; 
 		ctrRaderChart.y = 500 ;
+		ctrRaderChart.isShowPercentValue = true ; 
 		this.addChild(ctrRaderChart);
+
+		let drawBgLineInfo: CtrRadarChartDrawBgLineInfo = { radius: 4 ,color: 0x00ff00,alpha: 1} ;
+		ctrRaderChart.SetDrawBgLineInfo(drawBgLineInfo);
+		let drawPropertyInfo: CtrRadarChartDrawPropertyInfo = { pointRadius: 10 ,pointColor: 0xff00ff,pointAlpha: 1 , lineRadius: 4 ,lineColor: 0xff00ff,lineAlpha: 1, bgColor: 0xff0000, bgAlpha: 0.5 };
+		ctrRaderChart.SetDrawPropertyInfo(drawPropertyInfo);
+		// ctrRaderChart.ValidateCtrNow();
+
+		
+		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 	}
 }
--- src/toolkits/controller/radar/CtrRadarChart.ts
+++ src/toolkits/controller/radar/CtrRadarChart.ts
@@ -1,4 +1,4 @@
-type CtrRadarChartPropertyInfo = {
+type CtrRadarChartDrawPropertyInfo = {
 	pointRadius: number ,
 	pointColor: number ,
 	pointAlpha: number ,
@@ -9,7 +9,7 @@ type CtrRadarChartPropertyInfo = {
 	bgAlpha: number 
 }
 
-type CtrRadarChartLineInfo = {
+type CtrRadarChartDrawBgLineInfo = {
 	radius: number ,
 	color: number ,
 	alpha: number ,
@@ -20,31 +20,47 @@ type CtrRadarChartPropertyData = {
 	name: string , 
 	currentValue: number ,
 	maxValue: number , 
-	color: number , 
+	nameColor: number , 
+	valueColor: number , 
+	labelSize: number 
 }
 
 /**
  * @brief 绘制属性雷达图
+ *  以圆心为锚点
+ * 
  *  支持矢量图绘制雷达图底图，连线，属性范围连线，底图，属性点
  *  即将支持图片贴图显示雷达图底图，连线，属性范围连线，底图，属性点
  */
 class CtrRadarChart extends eui.Component implements  eui.UIComponent {
+	private mCycles: number = 3 ; 	//m圈
+	private mRadius: number = 0; 	//正多边形外接圆半径
+	private mIsValidateImmediately: boolean = true ;
+	private mLabelDist: number = 30 ;
+	private mIsShowPercentValue: boolean = false ;  
+	private mValueToFixed: number = 0 ;  //成比例时小数点后保留位数
+	private mIsDrawPropertyPoint: boolean = false ; 
+	private mDrawPropertyInfo: CtrRadarChartDrawPropertyInfo = { pointRadius: 10 ,pointColor: 0x3D9028,pointAlpha: 1 , lineRadius: 4 ,lineColor: 0x609D56,lineAlpha: 1, bgColor: 0x76A677, bgAlpha: 0.5 } ; 
+	private mDrawBgLineInfo: CtrRadarChartDrawBgLineInfo = { radius: 4 ,color: 0xB1BEC8,alpha: 1} ;
+	private mDataArray: Array<CtrRadarChartPropertyData> = [] ; 
+
 	private mSourcePos: egret.Point = new egret.Point(0,0);
 	private mBgArray: Array<egret.Shape> = [] ; 
 	private mSides: number = 0 ;	//n变形
-	private mCycles: number = 0 ; 	//m圈
-	private mRadius: number = 0; 	//正多边形外接圆半径
 	private mLinePointArray: Array< Array <egret.Point> > = [] ;    //二维数组，[ 第几个环:[每个环里各个顶点坐标] ]
 	private mPropertyPointArray: Array<egret.Point> = [] ; 			//各个属性坐标
 	private mLineAngle: Array<number> = [] ;		//记录对应属性线的角度，用来计算对应点的位置
 	private mPI: number = Math.PI ; 
-	private mIsDrawPropertyPoint: boolean = false ; 
-	private mRadarChartPropertyInfo: CtrRadarChartPropertyInfo = { pointRadius: 10 ,pointColor: 0xff00ff,pointAlpha: 1 , lineRadius: 4 ,lineColor: 0xff00ff,lineAlpha: 1, bgColor: 0xff0000, bgAlpha: 0.5 } ; 
-	private mRadarChartLineInfo: CtrRadarChartLineInfo = { radius: 4 ,color: 0x00ff00,alpha: 1} ;
-	private mDataArray: Array<CtrRadarChartPropertyData> = [] ; 
-	private mLabelDist: number = 0 ; 
 
-	public constructor() {
+
+
+
+	/**
+	 * @param dataArray 属性数组
+	 * @param cycles 显示的正多边形宽度
+	 * @param radius 正多边形外接圆半径
+	 */
+	public constructor(dataArray: Array<CtrRadarChartPropertyData>, isValidateImmediately: boolean = true ,cycles: number = 3 , radius: number = 150 , drawPropertyInfo ?:CtrRadarChartDrawPropertyInfo ,drawBgLineInfo ?:CtrRadarChartDrawBgLineInfo) {
 		super();
 
 		this.skinName = `<?xml version="1.0" encoding="utf-8"?>
@@ -52,27 +68,73 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 			<w:Config id="15f06854cab" ></w:Config>
 		</e:Skin>`
 
-		this.mSides = 6 ; 
-		this.mCycles = 3 ; 
-		this.mRadius = 200 ;
-		this.mIsDrawPropertyPoint = true ;
-		this.mLabelDist = 30;
-		
-		let data1:CtrRadarChartPropertyData = { name: "胸", currentValue: 90 , maxValue: 100 , color: 0xeeffcc };
-		let data2 = { name: "屁股", currentValue: 80 , maxValue: 100 , color: 0xeeffcc };
-		let data3 = { name: "腰", currentValue: 60 , maxValue: 100 , color: 0xeeffcc };
-		let data4 = { name: "腿", currentValue: 20 , maxValue: 100 , color: 0xeeffcc };
-		let data5 = { name: "手", currentValue: 40 , maxValue: 100 , color: 0xeeffcc };
-		let data6 = { name: "脸", currentValue: 50 , maxValue: 100 , color: 0xeeffcc };
-		this.mDataArray.push(data1);
-		this.mDataArray.push(data2);
-		this.mDataArray.push(data3);
-		this.mDataArray.push(data4);
-		this.mDataArray.push(data5);
-		this.mDataArray.push(data6);
+		this.mIsValidateImmediately = isValidateImmediately;
+		this.mCycles = cycles ; 
+		this.mRadius = radius ;
 
+		if (drawPropertyInfo) {
+			this.mDrawPropertyInfo = drawPropertyInfo;
+		}
+		if (drawBgLineInfo) {
+			this.mDrawBgLineInfo = drawBgLineInfo ; 
+		}
+		// this.mSides = 6 ; 
+		// this.mIsDrawPropertyPoint = true ;
+		// this.mLabelDist = 30;
+		
+		// let data1:CtrRadarChartPropertyData = { name: "胸", currentValue: 90 , maxValue: 100 , color: 0xeeffcc };
+		// let data2 = { name: "屁股", currentValue: 80 , maxValue: 100 , color: 0xeeffcc };
+		// let data3 = { name: "腰", currentValue: 60 , maxValue: 100 , color: 0xeeffcc };
+		// let data4 = { name: "腿", currentValue: 20 , maxValue: 100 , color: 0xeeffcc };
+		// let data5 = { name: "手", currentValue: 40 , maxValue: 100 , color: 0xeeffcc };
+		// let data6 = { name: "脸", currentValue: 50 , maxValue: 100 , color: 0xeeffcc };
+		// this.mDataArray.push(data1);
+		// this.mDataArray.push(data2);
+		// this.mDataArray.push(data3);
+		// this.mDataArray.push(data4);
+		// this.mDataArray.push(data5);
+		// this.mDataArray.push(data6);
+
+		this.setDataArray(dataArray);
+	}
+
+	public SetCycles(cycles: number): void { this.mCycles = cycles ; }
+	public SetRadius(radius: number): void { this.mRadius = radius ; }
+	public SetDrawBgLineInfo(info: CtrRadarChartDrawBgLineInfo): void { this.mDrawBgLineInfo = info; }
+	public SetDrawPropertyInfo(info: CtrRadarChartDrawPropertyInfo): void { this.mDrawPropertyInfo = info; }
+
+	public set isDrawPropertyPoint(isDraw: boolean ) { this.mIsDrawPropertyPoint = isDraw; }
+	public get isDrawPropertyPoint():boolean { return this.mIsDrawPropertyPoint ; }
+
+	public set isValidateImmediately(isValidate: boolean ) { this.mIsValidateImmediately = isValidate; }
+	public get isValidateImmediately():boolean { return this.mIsValidateImmediately ; }
+
+	public set isShowPercentValue(isShowPercent: boolean ) { this.mIsShowPercentValue = isShowPercent; }
+	public get isShowPercentValue():boolean { return this.mIsShowPercentValue ; }
+
+	public set valueToFixed(value: number ) { this.mValueToFixed = value; }
+	public get valueToFixed():number { return this.mValueToFixed ; }
+
+	/**
+	 * 属性相对于点的距离
+	 */
+	public set labelDist(value: number ) { this.mLabelDist = value; }
+	public get labelDist():number { return this.mLabelDist ; }
+
+	private setDataArray(dataArray: Array<CtrRadarChartPropertyData>): void {
+		for (let i = 0 ; i < dataArray.length ; ++i) {
+			this.mDataArray.push({ name: dataArray[i].name, 
+									currentValue: dataArray[i].currentValue , 
+									maxValue: dataArray[i].maxValue , 
+									nameColor: dataArray[i].nameColor, 
+									valueColor: dataArray[i].valueColor ,
+									labelSize: dataArray[i].labelSize});
+		}
 
 	}
+
+
+
 
 	protected partAdded(partName:string,instance:any):void
 	{
@@ -83,11 +145,14 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 	protected childrenCreated():void
 	{
 		super.childrenCreated();
-		this.init();
+		this.preInit();
+
+		if (this.mIsValidateImmediately) 
+			this.ValidateCtrNow();
 	}
 
-	private init(): void {
-		this.preInit();
+	public ValidateCtrNow(): void {
+		this.clear();
 
 		this.generateRadarPos();
 		this.generatePropertyPos();
@@ -100,6 +165,11 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 	private preInit(): void {
 		this.width = this.mRadius*2;
 		this.height = this.mRadius*2;
+		this.mSides = this.mDataArray.length;
+	}
+
+	private clear(): void {
+		this.removeChildren();
 	}
 
 	private generateRadarPos(): void {
@@ -111,8 +181,8 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 			for (let iSides = 0 ; iSides < this.mSides; ++iSides) {
 				let angle: number = iSides* 2*pi/this.mSides + disAngle;
 				let point: egret.Point = new egret.Point();
-				point.x = Math.cos(angle) * (cyclesR*(iCycles + 1)) ;		//排除原点作为了第一圈	
-				point.y = Math.sin(angle) * (cyclesR*(iCycles + 1)) ;
+				point.x = parseFloat( Math.cos(angle).toFixed(5)) * (cyclesR*(iCycles + 1)) ;		//排除原点作为了第一圈	
+				point.y = parseFloat( Math.sin(angle).toFixed(5)) * (cyclesR*(iCycles + 1)) ;
 				arrayPoint.push(point);
 
 				if (iCycles == 0) {
@@ -146,7 +216,7 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 	private drawBgRadarCycle(): void {
 		for (let iCycles = 0 ; iCycles < this.mCycles; ++iCycles) {
 			let polygon: egret.Shape = new egret.Shape();
-			polygon.graphics.lineStyle(this.mRadarChartLineInfo.radius , this.mRadarChartLineInfo.color);
+			polygon.graphics.lineStyle(this.mDrawBgLineInfo.radius , this.mDrawBgLineInfo.color);
 			polygon.graphics.moveTo(this.mLinePointArray[iCycles][0].x , this.mLinePointArray[iCycles][0].y);
 			for (let iSides = 0 ; iSides < this.mSides; ++iSides) {
 				polygon.graphics.lineTo(this.mLinePointArray[iCycles][iSides].x , this.mLinePointArray[iCycles][iSides].y);
@@ -161,7 +231,7 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 	private drawBgRadarLinkLine(): void {
 		for (let iSides = 0 ; iSides < this.mSides ; ++iSides) {
 			let line: egret.Shape = new egret.Shape();
-			line.graphics.lineStyle(this.mRadarChartLineInfo.radius , this.mRadarChartLineInfo.color);
+			line.graphics.lineStyle(this.mDrawBgLineInfo.radius , this.mDrawBgLineInfo.color);
 			line.graphics.moveTo(this.mSourcePos.x , this.mSourcePos.y);
 			line.graphics.lineTo(this.mLinePointArray[this.mCycles-1][iSides].x , this.mLinePointArray[this.mCycles-1][iSides].y);
 			line.graphics.endFill();
@@ -172,8 +242,8 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 	private drawPropertyInfo(): void {
 		//draw polygon
 		let polygon: egret.Shape = new egret.Shape();
-		polygon.graphics.lineStyle(this.mRadarChartPropertyInfo.lineRadius , this.mRadarChartPropertyInfo.lineColor);
-		polygon.graphics.beginFill( this.mRadarChartPropertyInfo.bgColor, this.mRadarChartPropertyInfo.bgAlpha);
+		polygon.graphics.lineStyle(this.mDrawPropertyInfo.lineRadius , this.mDrawPropertyInfo.lineColor);
+		polygon.graphics.beginFill( this.mDrawPropertyInfo.bgColor, this.mDrawPropertyInfo.bgAlpha);
 		polygon.graphics.moveTo(this.mPropertyPointArray[0].x , this.mPropertyPointArray[0].y);
 		for (let iSides = 0 ; iSides < this.mPropertyPointArray.length; ++iSides) {
 			polygon.graphics.lineTo(this.mPropertyPointArray[iSides].x , this.mPropertyPointArray[iSides].y);
@@ -186,9 +256,9 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 			for (let iSides = 0 ; iSides < this.mSides; ++iSides) {
 				this.createShapPointCricle(this.mPropertyPointArray[iSides].x , 
 											this.mPropertyPointArray[iSides].y , 
-											this.mRadarChartPropertyInfo.pointRadius , 
-											this.mRadarChartPropertyInfo.pointColor, 
-											this.mRadarChartPropertyInfo.pointAlpha);
+											this.mDrawPropertyInfo.pointRadius , 
+											this.mDrawPropertyInfo.pointColor, 
+											this.mDrawPropertyInfo.pointAlpha);
 			}
 		}
 
@@ -199,16 +269,29 @@ class CtrRadarChart extends eui.Component implements  eui.UIComponent {
 			point.y = point.y > this.mSourcePos.y ? point.y + this.mLabelDist : point.y - this.mLabelDist;
 			
 			let labelName: eui.Label = new eui.Label();
-			labelName.text = this.mDataArray[iSides].name;
-			labelName.x = point.x ; 
-			labelName.y = point.y ;
 			this.addChild(labelName);
+			labelName.size = this.mDataArray[iSides].labelSize;
+			labelName.text = this.mDataArray[iSides].name;
+			labelName.textAlign = point.x > this.mSourcePos.x ? egret.HorizontalAlign.LEFT : egret.HorizontalAlign.RIGHT;
+			labelName.x = labelName.textAlign == egret.HorizontalAlign.LEFT ? point.x : point.x - labelName.width; 
+			labelName.y = point.y > this.mSourcePos.y ? point.y - labelName.height : point.y ;
+			labelName.textColor = this.mDataArray[iSides].nameColor;
 
+			let value: string = "";
+			if (this.mIsShowPercentValue) {
+				value = (this.mDataArray[iSides].currentValue/this.mDataArray[iSides].maxValue * 100).toFixed(this.mValueToFixed) + "%";
+			}
+			else {
+				value = this.mDataArray[iSides].currentValue + "/" + this.mDataArray[iSides].maxValue;
+			}
 			let labelValue: eui.Label = new eui.Label();
-			labelValue.text = this.mDataArray[iSides].currentValue + "/" + this.mDataArray[iSides].maxValue;
-			labelValue.x = point.x ;
-			labelValue.y = point.y + 30;
 			this.addChild(labelValue);
+			labelValue.size = this.mDataArray[iSides].labelSize;
+			labelValue.textAlign = point.x > this.mSourcePos.x ? egret.HorizontalAlign.LEFT : egret.HorizontalAlign.RIGHT;
+			labelValue.text = value;
+			labelValue.x = labelValue.textAlign == egret.HorizontalAlign.LEFT ? point.x : point.x - labelValue.width;
+			labelValue.y = labelName.y + labelName.height;
+			labelValue.textColor = this.mDataArray[iSides].valueColor;
 		}
 	}
 
