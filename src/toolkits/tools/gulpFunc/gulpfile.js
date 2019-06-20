5bc5ba61bb3a3506631b97bbf13356653399f80a
zhiwei.liu <zhiwei.liu@funplus.com>
2019-06-20 15:22:35 +0800

    增加计算gulp

---
+++ src/toolkits/tools/gulpFunc/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+// const del = require('del');
+// const shell = require('gulp-shell');
+// const fs = require('fs');
+
+
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask
+
