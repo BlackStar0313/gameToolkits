9e17149dc120cbed13b39ad1a46a8ab92bd9efd7
zhiwei.liu <zhiwei.liu@funplus.com>
2019-06-20 16:25:49 +0800

    增加读取和写数据的方式

--- src/toolkits/tools/gulpFunc/gulpfile.js
+++ src/toolkits/tools/gulpFunc/gulpfile.js
@@ -1,13 +1,31 @@
 const gulp = require('gulp');
 // const del = require('del');
 // const shell = require('gulp-shell');
-// const fs = require('fs');
+const fs = require('fs');
+const json2xls = require('json2xls');
+
+let info = {};
 
 
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+gulp.task('readFile', (cb)=>{
+    let jsFile = './data/shop.json';
+    fs.readFile(jsFile, 'utf8', (err, data) => {
+        info = JSON.parse(data);
+        cb();
+    })
+})
 
-exports.default = defaultTask
+gulp.task('writeResult', (cb) => {
+     let jsonArr = [];
+     for (let key in info) {
+        jsonArr.push(info[key]);
+     }
 
+    var xls = json2xls(jsonArr);
+    fs.writeFileSync('./out/data.xlsx', xls, 'binary');
+     cb();
+})
+
+gulp.task('test',gulp.series('readFile', 'writeResult', (cb)=>{
+    cb();
+}))
